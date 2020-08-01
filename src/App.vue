68f383fe5f91e852a206658007db2e3e97b5d792
Yuri Duque <yurithielmann83@gmail.com>
2020-07-31 23:02:06 -0300

    criando tela de login

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import themeConfig from '@/../themeConfig.js'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  watch: {
+    '$store.state.theme'(val) {
+      this.toggleClassInBody(val);
+    }
+  },
+  methods: {
+    toggleClassInBody(className) {
+      if (className == 'dark') {
+        if (document.body.className.match('theme-semi-dark')) document.body.classList.remove('theme-semi-dark');
+        document.body.classList.add('theme-dark');
+      } else if (className == 'semi-dark') {
+        if (document.body.className.match('theme-dark')) document.body.classList.remove('theme-dark');
+        document.body.classList.add('theme-semi-dark');
+      } else {
+        if (document.body.className.match('theme-dark')) document.body.classList.remove('theme-dark');
+        if (document.body.className.match('theme-semi-dark')) document.body.classList.remove('theme-semi-dark');
+      }
+    },
+    handleWindowResize(event) {
+      this.$store.dispatch('updateWindowWidth', event.currentTarget.innerWidth);
+    },
+  },
+  mounted() {
+    this.toggleClassInBody(themeConfig.theme)
+    this.$nextTick(() => {
+      window.addEventListener('resize', this.handleWindowResize);
+    });
+    this.$store.dispatch('updateWindowWidth', window.innerWidth);
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.handleWindowResize);
+  },
 }
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
---
+++ src/views/pages/Login.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="h-screen flex w-full bg-img vx-row no-gutter items-center justify-center" id="page-login">
+        <div class="vx-col sm:m-0 m-4">
+            <vx-card>
+                <div slot="no-body" class="full-page-bg-color">
+                    <div class="vx-row no-gutter justify-center items-center">
+                        <div class="vx-col sm:w-full md:w-full d-theme-dark-bg">
+                            <div class="p-8">
+                                <div class="vx-card__title mb-8">
+                                    <h4 class="mb-4">Login</h4>
+                                    <p>Bora começar a gerenciar seu dinheiro?!</p>
+                                </div>
+                                <vs-input
+                                    icon="icon icon-user"
+                                    icon-pack="feather"
+                                    label-placeholder="Username"
+                                    v-model="username"
+                                    class="w-full no-icon-border"/>
+
+                                <vs-input
+                                    type="password"
+                                    icon="icon icon-lock"
+                                    icon-pack="feather"
+                                    label-placeholder="Senha"
+                                    v-model="password"
+                                    class="w-full mt-6 no-icon-border" />
+
+                                <div class="flex flex-wrap justify-between my-5">
+                                    <vs-checkbox v-model="checkbox_remember_me" class="mb-3">Lembre-me</vs-checkbox>
+                                    <router-link to="#">Esqueceu sua senha?</router-link>
+                                </div>
+                                <vs-button type="border">Cadastrar</vs-button>
+                                <vs-button class="float-right">Login</vs-button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </vx-card>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            username: '',
+            password: '',
+            checkbox_remember_me: false
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+</style>
