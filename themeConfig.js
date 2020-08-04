f47ba4af7f0e1d9632f500b8a02a429926f43780
Yuri Duque <yurithielmann83@gmail.com>
2020-08-03 22:56:28 -0300

    alterando cores do projeto

--- themeConfig.js
+++ themeConfig.js
@@ -1,17 +1,14 @@
-/*=========================================================================================
-  File Name: themeConfig.js
-  Description: Theme configuration
-  ----------------------------------------------------------------------------------------
-  Item Name: Vuesax Admin - Clean Vue.js Dashboard Admin Template
-  Author: Pixinvent
-  Author URL: hhttp://www.themeforest.net/user/pixinvent
-==========================================================================================*/
-
 // MAIN COLORS - VUESAX THEME COLORS
 let colors = {
-	primary: '#7367F0',
-	success: '#28C76F',
-	danger: '#EA5455',
+	// primary: '#7367F0',
+	// success: '#28C76F',
+	// danger: '#EA5455',
+	// warning: '#FF9F43',
+	// dark: '#1E1E1E',
+
+	primary: '#9b92f7',
+	success: '#43e88d',
+	danger: '#fc5354',
 	warning: '#FF9F43',
 	dark: '#1E1E1E',
 }
@@ -23,7 +20,7 @@ Vue.use(Vuesax, { theme:{ colors } });
 
 // CONFIGS
 const themeConfig = {
-	theme: 'light',						        // options[String]: 'light'(default), 'dark', 'semi-dark'
+	theme: 'dark',						        // options[String]: 'light'(default), 'dark', 'semi-dark'
 	sidebarCollapsed: false,			    // options[Boolean]: true, false(default)
 	navbarColor: "#fff",				      // options[String]: HEX color / rgb / rgba / Valid HTML Color name - (default: #fff)
 	navbarType: "floating",				    // options[String]: floating(default) / static / sticky / hidden
