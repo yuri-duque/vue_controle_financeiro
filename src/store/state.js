d5a443afbf32213fe5e3dd8b0a5a6ae1ddbce90d
Yuri Duque <yurithielmann83@gmail.com>
2020-08-03 22:56:01 -0300

    removendo comentarios

--- src/store/state.js
+++ src/store/state.js
@@ -1,12 +1,3 @@
-/*=========================================================================================
-  File Name: state.js
-  Description: Vuex Store - state
-  ----------------------------------------------------------------------------------------
-  Item Name: Vuesax Admin - VueJS Dashboard Admin Template
-  Author: Pixinvent
-  Author URL: http://www.themeforest.net/user/pixinvent
-==========================================================================================*/
-
 import navbarSearchAndPinList from '@/layouts/components/navbarSearchAndPinList'
 import themeConfig from '@/../themeConfig.js'
 import colors from '@/../themeConfig.js'
