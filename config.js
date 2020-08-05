e500db54455c9c1b4dd072423ea995f3ff588786
Yuri Duque <yurithielmann83@gmail.com>
2020-08-04 22:01:43 -0300

    alterando arquivo .env

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
-const apiUrl = "https://localhost:44324/api";
-const siteUrl =  process.env.VUE_APP_ROOT_SITE;
-const domain = process.env.VUE_APP_DOMAIN;
-const domainApi = process.env.VUE_APP_DOMAIN_API;
+const apiUrl = process.env.VUE_APP_ROOT_API;
 var config = {
-    apiUrl,siteUrl,domain, domainApi
+    apiUrl
 }
 
 export default config;
