c33e6b454d0e17028f237e82b8ad859a95b50852
Yuri Duque <yurithielmann83@gmail.com>
2020-08-10 23:00:53 -0300

    configurando tela de listagem de contas

---
+++ src/components/custom/conta/ListaContas.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <vs-row vs-w="12">
+      <vs-col vs-type="flex" vs-lg="4" vs-sm="6" vs-xs="12">
+        <cardConta />
+      </vs-col>
+      <vs-col vs-type="flex" vs-lg="4" vs-sm="6" vs-xs="12">
+        <cardConta />
+      </vs-col>
+      <vs-col vs-type="flex" vs-lg="4" vs-sm="6" vs-xs="12">
+        <cardConta />
+      </vs-col>
+    </vs-row>
+  </div>
+</template>
+
+<script>
+import cardConta from "./CardConta.vue";
+
+export default {
+  components: {
+    cardConta,
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/pages/contas/ContaLista.vue
+++ src/views/pages/contas/ContaLista.vue
@@ -1,13 +1,32 @@
 <template>
-    <h3>Contas</h3>
+  <div>
+    <h3 class="ml-2">Contas</h3>
+
+    <div class="mt-10">
+      <vs-row vs-w="12">
+        <vs-col vs-type="flex" vs-lg="4" vs-sm="6" vs-xs="12">
+          <cardConta />
+        </vs-col>
+        <vs-col vs-type="flex" vs-lg="4" vs-sm="6" vs-xs="12">
+          <cardConta />
+        </vs-col>
+        <vs-col vs-type="flex" vs-lg="4" vs-sm="6" vs-xs="12">
+          <cardConta />
+        </vs-col>
+      </vs-row>
+    </div>
+  </div>
 </template>
 
 <script>
+import cardConta from "@/components/custom/conta/CardConta.vue";
+
 export default {
-    
-}
+  components: {
+    cardConta,
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
