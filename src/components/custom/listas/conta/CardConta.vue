5a03b4717cb1d44e89125cd6b65bfbb4468e4e0c
Yuri Duque <yurithielmann83@gmail.com>
2020-08-10 22:58:41 -0300

    criando card conta

--- src/components/custom/listas/conta/CardConta.vue
+++ src/components/custom/listas/conta/CardConta.vue
@@ -1,13 +1,20 @@
 <template>
-    
+  <vs-card class="d-theme-dark-bg mx-2">
+    <div slot="header">
+      <vs-row vs-type="flex" vs-justify="center">
+        <h3>Carteira</h3>
+      </vs-row>
+    </div>
+
+    <vs-row vs-type="flex" vs-justify="center">
+      <h3>R$ 10.000,00</h3>
+    </vs-row>
+  </vs-card>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
-
 </style>
