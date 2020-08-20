03b28eb2e472dff39682635d0e5e91c067a6e202
Yuri Duque <yurithielmann83@gmail.com>
2020-08-19 23:15:45 -0300

    configurando metodos de consulta de contas

--- src/api/api_conta.js
+++ src/api/api_conta.js
@@ -1,4 +1,11 @@
 import apiUtils from "./api_utils.js";
 
-export default {    
+export default {   
+    getAll() {
+        return apiUtils.get("wallet");
+    },
+
+    save(conta) {
+        return apiUtils.post("wallet", conta);
+    }, 
 };
