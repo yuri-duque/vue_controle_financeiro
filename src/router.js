b9e48e74dae35e9ee3b291a85bcec1f71a9e333e
Yuri Duque <yurithielmann83@gmail.com>
2020-08-20 20:42:35 -0300

    configurando acl no projeto

--- src/router.js
+++ src/router.js
@@ -1,99 +1,101 @@
-/*=========================================================================================
-  File Name: router.js
-  Description: Routes for vue-router. Lazy loading is enabled.
-  ----------------------------------------------------------------------------------------
-  Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
-  Author: Pixinvent
-  Author URL: http://www.themeforest.net/user/pixinvent
-==========================================================================================*/
-
-
 import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
 
 const router = new Router({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    scrollBehavior () {
-        return { x: 0, y: 0 }
+  mode: 'history',
+  base: process.env.BASE_URL,
+  scrollBehavior() {
+    return { x: 0, y: 0 }
+  },
+  routes: [
+    {
+      path: "",
+      component: () => import('@/views/pages/usuario/Login.vue'),
+      meta: {
+        rule: "public"
+      },
     },
-    routes: [
 
+    {
+      path: '/',
+      name: 'board',
+      component: () => import('./layouts/main/Main.vue'),
+      children: [
         {
-            path: '',
-            component: () => import('./layouts/main/Main.vue'),
-            children: [
-              {
-                path: '/',
-                name: 'home',
-                component: () => import('./views/Home.vue'),
-                meta: {
-                  rule: "admin"
-                },
-              },
+          path: '',
+          name: 'home',
+          component: () => import('./views/Home.vue'),
+          meta: {
+            rule: "admin"
+          },
+        },
 
-              // CONTA
-              {
-                path: '/conta',
-                name: 'conta-lista',
-                component: () => import('./views/pages/contas/ContaLista.vue'),
-                meta: {
-                  rule: "admin"
-                },
-              },
-              {
-                path: '/conta/cadastro',
-                name: 'conta-cadastro',
-                component: () => import('./views/pages/contas/ContaForm.vue'),
-                meta: {
-                  rule: "admin"
-                },
-              },
-            ],
+        // CONTA
+        {
+          path: '/conta',
+          name: 'conta-lista',
+          component: () => import('./views/pages/contas/ContaLista.vue'),
+          meta: {
+            rule: "admin"
+          },
         },
         {
-            path: '',
-            component: () => import('@/layouts/full-page/FullPage.vue'),
-            children: [
-              {
-                path: '/login',
-                name: 'usuario-login',
-                component: () => import('@/views/pages/usuario/Login.vue'),
-                meta: {
-                  rule: "public"
-                },
-              },
-              {
-                path: '/cadastro',
-                name: 'usuario-cadastro',
-                component: () => import('@/views/pages/usuario/Cadastro.vue'),
-                meta: {
-                  rule: "public"
-                },
-              },
-              {
-                path: '/404',
-                name: '404',
-                component: () => import('@/views/pages/404.vue')
-              },
-            ]
+          path: '/conta/cadastro',
+          name: 'conta-cadastro',
+          component: () => import('./views/pages/contas/ContaForm.vue'),
+          meta: {
+            rule: "admin"
+          },
         },
-        // Redirect to 404 page, if no match found
+      ],
+    },
+
+    {
+      path: '',
+      component: () => import('@/layouts/full-page/FullPage.vue'),
+      children: [
         {
-            path: '*',
-            redirect: '404'
-        }
-    ],
+          path: '/login',
+          name: 'usuario-login',
+          component: () => import('@/views/pages/usuario/Login.vue'),
+          meta: {
+            rule: "public"
+          },
+        },
+        {
+          path: '/cadastro',
+          name: 'usuario-cadastro',
+          component: () => import('@/views/pages/usuario/Cadastro.vue'),
+          meta: {
+            rule: "public"
+          },
+        },
+        {
+          path: '/404',
+          name: '404',
+          component: () => import('@/views/pages/404.vue'),
+          meta: {
+            rule: "public"
+          },
+        },
+      ]
+    },
+    // Redirect to 404 page, if no match found
+    {
+      path: '*',
+      redirect: '404'
+    }
+  ],
 })
 
 router.afterEach(() => {
   // Remove initial loading
   const appLoading = document.getElementById('loading-bg')
-    if (appLoading) {
-        appLoading.style.display = "none";
-    }
+  if (appLoading) {
+    appLoading.style.display = "none";
+  }
 })
 
 export default router
