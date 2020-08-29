c07efae8af470aa8c03b07a0274f4a487d00ee01
Yuri Duque <yurithielmann83@gmail.com>
2020-08-28 22:01:37 -0300

    arrumando posiçoes dos botoes

--- src/components/custom/conta/CardConta.vue
+++ src/components/custom/conta/CardConta.vue
@@ -1,5 +1,5 @@
 <template>
-  <vs-card class="d-theme-dark-bg mx-2 m-0">
+  <vs-card class="d-theme-dark-bg pb-6 mx-4">
     <div slot="header">
       <vs-row vs-type="flex" vs-justify="center">
         <h3>{{titulo}}</h3>
@@ -9,16 +9,30 @@
     <vs-row vs-type="flex" vs-justify="center">
       <h3>{{formatarValor(valor)}}</h3>
     </vs-row>
-    
-    <vs-row class="my-3 mt-5">
-      <vs-col vs-type="flex" vs-w="6" class="px-2">
-        <vs-button icon-pack="feather" icon="icon-arrow-down" color="danger" class="w-full" @click="despesa"></vs-button>
-      </vs-col>
 
-      <vs-col vs-type="flex" vs-w="6" class="px-2">
-        <vs-button icon-pack="feather" icon="icon-arrow-up" color="success" class="w-full"  @click="receita"></vs-button>
-      </vs-col>      
-    </vs-row>
+    <div slot="footer">
+      <vs-row >
+        <vs-col vs-type="flex" vs-w="6" class="px-2">
+          <vs-button
+            icon-pack="feather"
+            icon="icon-arrow-down"
+            color="danger"
+            class="w-full"
+            @click="despesa"
+          ></vs-button>
+        </vs-col>
+
+        <vs-col vs-type="flex" vs-w="6" class="px-2">
+          <vs-button
+            icon-pack="feather"
+            icon="icon-arrow-up"
+            color="success"
+            class="w-full"
+            @click="receita"
+          ></vs-button>
+        </vs-col>
+      </vs-row>
+    </div>
   </vs-card>
 </template>
 
@@ -41,20 +55,16 @@ export default {
       return valueFormatado;
     },
 
-    despesa(){
-      this.$emit('despesa')
+    despesa() {
+      this.$emit("despesa");
     },
 
-    receita(){
-      this.$emit('receita')
-    }
+    receita() {
+      this.$emit("receita");
+    },
   },
 };
 </script>
 
 <style>
-.vs-card--content{
-  padding-bottom: 0px !important;
-  margin-bottom: 0px !important;
-}
 </style>
